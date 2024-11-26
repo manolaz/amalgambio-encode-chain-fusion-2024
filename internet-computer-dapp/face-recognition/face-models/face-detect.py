5e5c93bafc4c0b8a7e0e7b9e020533f128c8e563
Tristan NGUYEN <trungupdate@gmail.com>
2024-11-27 00:29:30 +0700

    clean

--- internet-computer-dapp/face-recognition/face-models/face-detect.py
+++
@@ -1,5 +0,0 @@
-import torch
-import facenet_pytorch
-resnet = facenet_pytorch.InceptionResnetV1(pretrained='vggface2').eval()
-input = torch.randn(1, 3, 160, 160)
-torch.onnx.export(resnet, input, "version-RFB-320.onnx", verbose=False, opset_version=11)
